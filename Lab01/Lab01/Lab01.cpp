9a865399a295fcc0796e55f02acf26744899fd1d
SWojcik800 <43492034+SWojcik800@users.noreply.github.com>
2021-10-13 16:24:53 +0200

    Fix switch * space bug

--- Lab01/Lab01/Lab01.cpp
+++ Lab01/Lab01/Lab01.cpp
@@ -105,7 +105,7 @@ void Z5() {
 			cout << _a + " - " + _b + " = " + result;
 			break;
 		}
-		case '* ':
+		case '*':
 		{
 			string result = to_string(a * b);
 			cout << _a + " * " + _b + " = " + result;
